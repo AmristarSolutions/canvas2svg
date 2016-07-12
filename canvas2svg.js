5f64f3b12bbfa31deff5a21e37e5ac771e4f86f5
Matthew Vlietstra <matthew@amristar.com>
2016-07-12 13:41:55 +0800

    Fixed crash in IE when rendering a large scene.

--- canvas2svg.js
+++ canvas2svg.js
@@ -418,12 +418,6 @@
         var serialized = new XMLSerializer().serializeToString(this.__root),
             keys, i, key, value, regexp, xmlns;
 
-        //IE search for a duplicate xmnls because they didn't implement setAttributeNS correctly
-        xmlns = /xmlns="http:\/\/www\.w3\.org\/2000\/svg".+xmlns="http:\/\/www\.w3\.org\/2000\/svg/gi;
-        if(xmlns.test(serialized)) {
-            serialized = serialized.replace('xmlns="http://www.w3.org/2000/svg','xmlns:xlink="http://www.w3.org/1999/xlink');
-        }
-
         if(fixNamedEntities) {
             keys = Object.keys(namedEntities);
             //loop over each named entity and replace with the proper equivalent.
